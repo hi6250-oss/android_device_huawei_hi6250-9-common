01fa14f685fb0391a57b4c0fcd130732974f8226
A2L5E0X1 <alexed25@protonmail.com>
2021-07-31 01:07:44 +0200

    hi6250: build bcm bluetooth hal
    Change-Id: If7d1f89e265722d9b0dd2032e2684516c5b70e9f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Bluetooth
+BOARD_HAVE_BLUETOOTH_BCM := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := hi6250
 
