8ccdbe1eafeefe9d31d0dfbb21f2c4253ebfceb8
A2L5E0X1 <alexed25@protonmail.com>
2022-01-09 14:22:50 +0100

    hi6250: hack to bypass xml checks of native_packages.xml
    * vendor/huawei/hi6250-9-common/proprietary/vendor/etc/native_packages.xml:1: parser error : Start tag expected, '<' not found
    
    Change-Id: I8ff628b420bd469a79fcdac190e92a5a3f8de6d4

--- Android.mk
+++ Android.mk
@@ -26,6 +26,11 @@ $(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
+NATIVE_PACKAGES_FIXUP := $(TARGET_OUT_VENDOR)/etc/native_packages.xml
+$(NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/native_packages.bin
+	@echo "Move native_packages.bin to native_packages.xml"
+	$(hide) mv $(TARGET_OUT_VENDOR)/etc/native_packages.bin $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS) $(NATIVE_PACKAGES_FIXUP)
 
 endif
