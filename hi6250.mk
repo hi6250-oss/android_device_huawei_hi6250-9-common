de9decabcb62f2f4be6f3a71fbe526fb82ec4d7a
A2L5E0X1 <alexed25@protonmail.com>
2022-03-13 12:23:11 +0000

    hi6250: update packages for audio
    Change-Id: Iee55242ecc90c42d34539ee7f1032288952ec1d3

--- hi6250.mk
+++ hi6250.mk
@@ -16,11 +16,9 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@4.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
