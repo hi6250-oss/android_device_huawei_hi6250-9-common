3266b478a9d3c3b8991f812e89c623301ea17234
A2L5E0X1 <alexed25@protonmail.com>
2022-03-18 10:11:57 +0000

    hi6250: hidl: import default audio service
    Change-Id: I9b93bbdcd24dd7f2b44f25ac1fd32b095da889b3

--- hi6250.mk
+++ hi6250.mk
@@ -18,7 +18,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.service \
+    android.hardware.audio.service_64 \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
